3b2d94d2db368ec8f438580c870a2fcf2864877f
llamato <llamato@users.noreply.github.com>
2025-10-06 04:51:47 +0200

    Modified flake.nix for installability

--- flake.nix
+++ flake.nix
@@ -1,15 +1,41 @@
 {
-    description = "An attempt at a simple nix dev shell and build script";
-    inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  description = "An attempt at a simple nix dev shell and build script";
+  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    outputs = { self, nixpkgs-unstable }:
+  outputs = { self, nixpkgs-unstable }:
     let
-      pkgs = import nixpkgs-unstable { system = "x86_64-linux"; };
+      # Support multiple systems
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs-unstable.lib.genAttrs systems;
+      pkgsFor = system: import nixpkgs-unstable { inherit system; };
     in
     {
-    pkgs.haskellPackages.developPackage {
-      root = ./.;
-      name = "gcalc";
+      # Define packages
+      packages = forAllSystems (system:
+        let pkgs = pkgsFor system;
+        in {
+          default = pkgs.haskellPackages.developPackage {
+            root = ./.;
+            name = "gcalc";
+          };
+        }
+      );
+
+      # Define development shell
+      devShells = forAllSystems (system:
+        let pkgs = pkgsFor system;
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # Add Haskell development tools
+              haskellPackages.ghc
+              haskellPackages.cabal-install
+              haskellPackages.haskell-language-server
+              haskellPackages.hlint
+              haskellPackages.ormolu
+            ];
+          };
+        }
+      );
     };
-  }
 }
